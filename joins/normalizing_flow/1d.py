db051cc9d7ecf051bd3bf066f311261612f2690d
quincyma <quincyma@tencent.com>
2023-10-22 14:51:55 +0800

    scale data

--- joins/normalizing_flow/1d.py
+++ joins/normalizing_flow/1d.py
@@ -5,7 +5,7 @@ import torch.utils.data as data
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.distributions.uniform import Uniform
-from torch.distributions.beta import Beta
+from sklearn import preprocessing
 
 
 class Nflow1D:
@@ -17,6 +17,7 @@ class Nflow1D:
         self.min = None
         self.max = None
         self.target_distribution = None
+        self.scaler = None
 
         device = 'cpu'
         if enable_cuda:
@@ -29,11 +30,12 @@ class Nflow1D:
             self.device), torch.tensor(1.0).to(self.device))
 
     def fit(self, x):
-        x = x.astype('float32')
         self.min = np.min(x)
         self.max = np.max(x)
+        self.scaler = preprocessing.StandardScaler()
+        x_scaled = self.scaler.fit_transform(x)
         train_loader = data.DataLoader(NumpyDataset(
-            x, self.device), batch_size=128, shuffle=True)
+            x_scaled, self.device), batch_size=128, shuffle=True)
 
         self.pdf, train_losses = train_and_eval(
             50, 5e-3, train_loader, self.target_distribution, self.device)
@@ -45,13 +47,14 @@ class Nflow1D:
             plt.show()
 
     def predict(self, x):
-        z, dz_by_dx = self.pdf(torch.FloatTensor(x).to(self.device))
+        z, dz_by_dx = self.pdf(torch.FloatTensor(
+            self.scaler.transform(x)).to(self.device))
         px = torch.exp(self.target_distribution.log_prob(z) +
                        dz_by_dx.log()).detach().to('cpu').numpy()
         return px
 
     def plot(self):
-        x = np.linspace(self.min, self.max, 100)
+        x = np.linspace(self.min, self.max, 100).reshape(-1, 1)
         px = self.predict(x)
         # print(x)
         # print(px)
@@ -137,57 +140,10 @@ class NumpyDataset(data.Dataset):
 
 
 if __name__ == '__main__':
-    # device = 'cpu'
-    # enable_cuda = True
-    # if enable_cuda:
-    #     if torch.cuda.is_available():
-    #         device = 'cuda'
-    #     elif torch.backends.mps.is_available():
-    #         device = 'mps'
-    # device = torch.device(device)
-
-    n_train, n_test = 20000, 1000
+    n_train, n_test = 200, 1000
     train_data = generate_mixture_of_gaussians(n_train)
-    # print(train_data)
-    # print(type(train_data))
-    # test_data = generate_mixture_of_gaussians(n_test)
-
-    # train_loader = data.DataLoader(NumpyDataset(
-    #     train_data, device), batch_size=128, shuffle=True)
-    # test_loader = data.DataLoader(NumpyDataset(
-    #     test_data), batch_size=128, shuffle=True)
-
-    # _, axes = plt.subplots(1, 2, figsize=(12, 4))
-    # _ = axes[0].hist(train_loader.dataset.array, bins=50)
-    # # _ = axes[1].hist(test_loader.dataset.array, bins=50)
-    # _ = axes[0].set_title('Training data')
-    # _ = axes[1].set_title('Testing data')
-    # plt.show()
-
-    flow = Nflow1D(enable_cuda=False)
+    train_data = train_data.reshape(-1, 1)
+    print(train_data)
+    flow = Nflow1D(enable_cuda=True)
     flow.fit(train_data)
     flow.plot()
-
-    # target_distribution = Uniform(0.0, 1.0)
-    # flow, train_losses = train_and_eval(
-    #     50, 5e-3, train_loader, target_distribution)
-
-    # _ = plt.plot(train_losses, label='train_loss')
-    # # _ = plt.plot(test_losses, label='test_loss')
-    # plt.legend()
-    # plt.show()
-
-    # x = np.linspace(-3, 3, 1000)
-    # with torch.no_grad():
-    #     z, dz_by_dx = flow(torch.FloatTensor(x))
-    #     px = (target_distribution.log_prob(z) +
-    #           dz_by_dx.log()).exp().cpu().numpy()
-
-    # _, axes = plt.subplots(1, 2, figsize=(12, 4))
-    # _ = axes[0].grid(), axes[1].grid()
-    # _ = axes[0].plot(x, px)
-    # _ = axes[0].set_title('Learned probability distribution')
-
-    # _ = axes[1].plot(x, z)
-    # _ = axes[1].set_title('x -> z')
-    # plt.show()
