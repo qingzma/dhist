cd9dfd39584ef2bddd7ef3a6a92eca1bdd3370ad
quincyma <quincyma@tencent.com>
2024-06-04 21:40:19 +0800

    schema info

---
+++ workloads/stats_CEB/helper.sql
@@ -0,0 +1,14 @@
+-- U.Id=b.UserId=c.UserId=ph.UserId=v.UserId=p.OwnerUserId
+select min(id),max(id) from users;
+select min(userid),max(userid) from badges;
+select min(userid),max(userid) from comments;
+select min(userid),max(userid) from postHistory;
+select min(userid),max(userid) from votes;
+select min(OwnerUserId),max(OwnerUserId) from posts;
+-- final is [-1, 55747]
+
+
+-- p.Id=pl.PostId=c.PostId=ph.PostId=v.PostId
+--     =pl.RelatedPostId
+select min(id),max(id) from posts;
+--  [1 , 115378]
