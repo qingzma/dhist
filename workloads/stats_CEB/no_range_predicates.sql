4d0536b5b74949cb83af813d93f26c17cf1e607e
李屿山 <308824783@qq.com>
2024-06-10 17:06:43 +0800

    save p.Id=pl.PostId=c.PostId=ph.PostId=v.PostId=t.ExcerptPostId

--- workloads/stats_CEB/no_range_predicates.sql
+++ workloads/stats_CEB/no_range_predicates.sql
@@ -42,6 +42,15 @@ SELECT COUNT(*) FROM  users as u, badges as b, comments as c, postHistory as ph 
 SELECT COUNT(*) FROM  users as u, comments as c, votes as v,  posts as p         WHERE  u.Id = v.UserId AND u.Id=c.UserId AND u.Id=p.OwnerUserId;
 SELECT COUNT(*) FROM  users as u, comments as c, votes as v,  postHistory as ph  WHERE  u.Id = v.UserId AND u.Id=c.UserId AND u.Id=ph.UserId;
 SELECT COUNT(*) FROM  users as u, votes as v,  posts as p, postHistory as ph     WHERE u.Id = v.UserId AND u.Id=p.OwnerUserId AND u.Id=ph.UserId;
+SELECT COUNT(*) FROM  users as u, badges as b, votes as v, posts as p            WHERE u.Id = b.UserId AND u.Id=v.UserId AND u.Id=p.OwnerUserId;
+SELECT COUNT(*) FROM  users as u, badges as b, votes as v, postHistory as ph     WHERE u.Id = b.UserId AND u.Id=v.UserId AND u.Id=ph.UserId;
+SELECT COUNT(*) FROM  users as u, badges as b, posts as p, postHistory as ph     WHERE u.Id = b.UserId AND u.Id=p.OwnerUserId AND u.Id=ph.UserId;
+SELECT COUNT(*) FROM  users as u, comments as c, posts as p, postHistory as ph   WHERE u.Id = c.UserId AND u.Id=p.OwnerUserId AND u.Id=ph.UserId;
+SELECT COUNT(*) FROM  badges as b, comments as c, votes as v, postHistory as ph  WHERE b.UserId = c.UserId AND b.UserId=v.UserId AND b.UserId=ph.UserId;
+SELECT COUNT(*) FROM  badges as b, comments as c, votes as v, posts as p         WHERE b.UserId = c.UserId AND b.UserId=v.UserId AND b.UserId=p.OwnerUserId;
+SELECT COUNT(*) FROM  badges as b, comments as c, postHistory as ph, posts as p  WHERE b.UserId = c.UserId AND b.UserId=p.OwnerUserId AND b.UserId=ph.UserId;
+SELECT COUNT(*) FROM  badges as b, votes as v, posts as p, postHistory as ph      WHERE b.UserId = v.UserId AND b.UserId=p.OwnerUserId AND b.UserId=ph.UserId;
+SELECT COUNT(*) FROM  comments as c, votes as v, posts as p, postHistory as ph   WHERE c.UserId = v.UserId AND c.UserId=p.OwnerUserId AND c.UserId=ph.UserId;
 
 
 SELECT COUNT(*) FROM  users as u, badges as b, comments as c, votes as v, posts as p           WHERE  u.Id = b.UserId AND u.Id=c.UserId AND u.Id=v.UserId AND u.Id=p.OwnerUserId;
@@ -51,6 +60,69 @@ SELECT COUNT(*) FROM  users as u, comments as c, votes as v, posts as p , postHi
 
 
 SELECT COUNT(*) FROM  users as u, badges as b, comments as c, votes as v, posts as p, postHistory as ph    WHERE  u.Id = b.UserId AND u.Id=c.UserId AND u.Id=v.UserId AND u.Id=p.OwnerUserId  AND u.Id=ph.UserId;
+
+-- pl.PostId
+SELECT COUNT(*) FROM  posts as p, postLinks as pl         WHERE  p.Id = pl.PostId;
+SELECT COUNT(*) FROM  posts as p, comments as c           WHERE  p.Id = c.PostId;
+SELECT COUNT(*) FROM  posts as p, postHistory as ph       WHERE  p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, votes as v              WHERE  p.Id = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c      WHERE  pl.PostId = c.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, postHistory as ph  WHERE  pl.PostId = ph.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, votes as v         WHERE  pl.PostId = v.PostId;
+SELECT COUNT(*) FROM  comments as c, postHistory as ph    WHERE  c.PostId = ph.PostId;
+SELECT COUNT(*) FROM  comments as c, votes as v           WHERE  c.PostId = v.PostId;
+SELECT COUNT(*) FROM  postHistory as ph, votes as v       WHERE  ph.PostId = v.PostId;
+
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c              WHERE  p.Id = pl.PostId AND p.Id = c.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, postHistory as ph          WHERE  p.Id = pl.PostId AND p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, votes as v                 WHERE  p.Id = pl.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, comments as c, postHistory as ph            WHERE  p.Id = c.PostId AND p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, comments as c, votes as v                   WHERE  p.Id = c.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, postHistory as ph, votes as v               WHERE  p.Id = ph.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c, postHistory as ph       WHERE  pl.PostId = ph.PostId AND pl.PostId = c.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c, votes as v              WHERE  pl.PostId = c.PostId AND pl.PostId = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, postHistory as ph, votes as v          WHERE  pl.PostId = ph.PostId AND pl.PostId = v.PostId;
+SELECT COUNT(*) FROM  comments as c, postHistory as ph, votes as v            WHERE  c.PostId = ph.PostId AND c.PostId = v.PostId;
+
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c, postHistory as ph     WHERE  p.Id = pl.PostId AND p.Id = c.PostId AND p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c, votes as v            WHERE  p.Id = pl.PostId AND p.Id = c.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, postHistory as ph, votes as v        WHERE  p.Id = pl.PostId AND p.Id = ph.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, comments as c, postHistory as ph, votes as v          WHERE  p.Id = c.PostId AND p.Id = ph.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c, postHistory as ph, votes as v     WHERE  pl.PostId = c.PostId AND pl.PostId = ph.PostId AND pl.PostId = v.PostId;
+
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c, postHistory as ph, votes as v      WHERE  p.Id = pl.PostId AND p.Id = c.PostId AND p.Id = ph.PostId AND p.Id = v.PostId;
+
+-- pl.RelatedPostId
+SELECT COUNT(*) FROM  posts as p, postLinks as pl         WHERE  p.Id = pl.RelatedPostId;
+SELECT COUNT(*) FROM  posts as p, comments as c           WHERE  p.Id = c.PostId;
+SELECT COUNT(*) FROM  posts as p, postHistory as ph       WHERE  p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, votes as v              WHERE  p.Id = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c      WHERE  pl.RelatedPostId = c.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, postHistory as ph  WHERE  pl.RelatedPostId = ph.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, votes as v         WHERE  pl.RelatedPostId = v.PostId;
+SELECT COUNT(*) FROM  comments as c, postHistory as ph    WHERE  c.PostId = ph.PostId;
+SELECT COUNT(*) FROM  comments as c, votes as v           WHERE  c.PostId = v.PostId;
+SELECT COUNT(*) FROM  postHistory as ph, votes as v       WHERE  ph.PostId = v.PostId;
+
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c              WHERE  p.Id = pl.RelatedPostId AND p.Id = c.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, postHistory as ph          WHERE  p.Id = pl.RelatedPostId AND p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, votes as v                 WHERE  p.Id = pl.RelatedPostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, comments as c, postHistory as ph            WHERE  p.Id = c.PostId AND p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, comments as c, votes as v                   WHERE  p.Id = c.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, postHistory as ph, votes as v               WHERE  p.Id = ph.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c, postHistory as ph       WHERE  pl.RelatedPostId = ph.PostId AND pl.RelatedPostId = c.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c, votes as v              WHERE  pl.RelatedPostId = c.PostId AND pl.RelatedPostId = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, postHistory as ph, votes as v          WHERE  pl.RelatedPostId = ph.PostId AND pl.RelatedPostId = v.PostId;
+SELECT COUNT(*) FROM  comments as c, postHistory as ph, votes as v            WHERE  c.PostId = ph.PostId AND c.PostId = v.PostId;
+
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c, postHistory as ph     WHERE  p.Id = pl.RelatedPostId AND p.Id = c.PostId AND p.Id = ph.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c, votes as v            WHERE  p.Id = pl.RelatedPostId AND p.Id = c.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, postHistory as ph, votes as v        WHERE  p.Id = pl.RelatedPostId AND p.Id = ph.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  posts as p, comments as c, postHistory as ph, votes as v          WHERE  p.Id = c.PostId AND p.Id = ph.PostId AND p.Id = v.PostId;
+SELECT COUNT(*) FROM  postLinks as pl, comments as c, postHistory as ph, votes as v     WHERE  pl.RelatedPostId = c.PostId AND pl.RelatedPostId = ph.PostId AND pl.RelatedPostId = v.PostId;
+
+SELECT COUNT(*) FROM  posts as p, postLinks as pl, comments as c, postHistory as ph, votes as v      WHERE  p.Id = pl.RelatedPostId AND p.Id = c.PostId AND p.Id = ph.PostId AND p.Id = v.PostId;
+
 
 
 
